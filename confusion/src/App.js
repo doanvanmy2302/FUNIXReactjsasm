3f425c4003f69634a5f5a501af0e762ed27fdc41
My <my.dv2002@gmail.com>
2022-01-09 11:04:03 +0700

    Presentational and Container component

---
+++ confusion/src/App.js
@@ -0,0 +1,16 @@
+
+import React, {Component} from 'react';
+import './App.css';
+
+import Main from './components/MainComponents';
+class App extends Component {
+  render () {
+
+  return (
+    <div >
+     <Main />
+    </div>
+  );}
+}
+
+export default App;
