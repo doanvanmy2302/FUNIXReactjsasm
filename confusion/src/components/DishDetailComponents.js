ae31f0256fe9ba717cb482e36bc30285204630ff
My <my.dv2002@gmail.com>
2022-01-09 15:12:48 +0700

    Header and Footer

---
+++ confusion/src/components/DishDetailComponents.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle } from 'reactstrap';
+
+    function RenderDish({dish}) {
+            return(
+                <div className="col-12 col-md-5 m-1">
+                        <Card>
+                            <CardImg src={ dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    
+                </div>
+            );
+    }
+
+    function RenderComments({comments}) {
+        if (comments != null)
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                            {comments.map((comment) => {
+                                return (
+                                    <div key={comment._id}>
+                                        <li>
+                                        <p>{comment.comment}</p>
+                                        <p>{comment.rating} stars</p>
+                                        <p>-- {comment.author.firstname} {comment.author.lastname} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(comment.updatedAt)))}</p>
+                                        </li>
+                                    </div>
+                                );
+                            })}
+                       
+                    </ul>
+                </div>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    const DishDetail = (props) => {
+        if (props.dish != null) {
+            return(
+                <div className="container">
+                    <div className="row">
+                   <RenderDish dish={props.dish} />
+                   <RenderComments comments={props.comments} />
+                    </div>
+                </div>
+            );
+        }
+        else
+            return(
+                <div></div>
+            );
+    }
+
+export default DishDetail;
