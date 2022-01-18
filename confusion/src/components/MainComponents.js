cae85c85d87b2ec2621842559befcc9fb1e682ce
My <my.dv2002@gmail.com>
2022-01-18 21:45:29 +0700

    Controlled Forms

---
+++ confusion/src/components/MainComponents.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import Header from './HeadComponents';
+import Home from './HomeComponents';
+import Footer from './FootComponents';
+import Contact from './ContactComponents';
+import DishDetail from './DishDetailComponents';
+import About from './AboutComponents';
+import { DISHES } from '../shared/dishes';
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
+      comments: COMMENTS,
+    };
+  }
+
+  render() {
+
+    const HomePage = () => {
+      return (<Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]} />)
+    }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Route path='/aboutus' component={() =><About leaders={this.state.leaders} />} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Main;
