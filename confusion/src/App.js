b64afa5ebd110408a086c83f187d2dcebace6226
My <my.dv2002@gmail.com>
2022-01-20 10:52:51 +0700

    React redux Form

---
+++ confusion/src/App.js
@@ -0,0 +1,23 @@
+
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import Main from './components/MainComponents';
+import {ConfigureStore} from './redux/configureStore';
+import {Provider} from 'react-redux';
+const store = ConfigureStore();
+class App extends Component {
+  render() {
+
+    return (
+      <Provider store={store}>
+      <BrowserRouter>
+        <div >
+          <Main />
+        </div></BrowserRouter>
+        </Provider>
+    );
+  }
+}
+
+export default App;
