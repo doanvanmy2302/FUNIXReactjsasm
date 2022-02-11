9589b5087ea55924bab7c718efe26a7bfee8dd57
My <my.dv2002@gmail.com>
2022-02-11 08:41:49 +0700

    react redux forms revisited

---
+++ confusion/src/components/MainComponents.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import Menu from './MenuComponent';
+import Header from './HeadComponents';
+import Home from './HomeComponents';
+import Footer from './FootComponents';
+import Contact from './ContactComponents';
+import DishDetail from './DishDetailComponents';
+import { actions } from 'react-redux-form';
+import About from './AboutComponents';
+import { Route, Switch, Redirect , withRouter} from 'react-router-dom';
+import { addComment, fetchDishes } from '../redux/ActionCreactors';
+import {connect} from 'react-redux'
+const mapStateToStore =state =>{
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+
+  }
+}
+
+const mapDispatchToProps = dispatch =>({
+  addComment: (dishId, rating, author,comment) => dispatch(addComment(dishId, rating, author,comment)),
+  fetchDishes: () => { dispatch(fetchDishes())},
+  resetFeedBack: () =>{dispatch(actions.reset('feedback'))}
+});
+
+class Main extends Component {
+ 
+componentDidMount() {
+  this.props.fetchDishes();
+}
+  render() {
+
+    const HomePage = () => {
+      return (<Home dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+      dishesLoading={this.props.dishes.isLoading}
+      dishesErrMess={this.props.dishes.errMess}
+        promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.props.leaders.filter((leader) => leader.featured)[0]} />)
+    }
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            addComment={this.props.addComment} />
+      );
+    };
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path="/contactus" component={() => <Contact resetFeedBack={this.props.resetFeedBack }/>} />
+          <Route path='/aboutus' component={() =><About leaders={this.props.leaders} />} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default withRouter(connect(mapStateToStore, mapDispatchToProps)(Main));
